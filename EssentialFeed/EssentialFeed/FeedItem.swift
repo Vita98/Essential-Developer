32df5398680584db1c71f869c5ed0226c46a09ac
Vitandrea Sorino <vitandrea98@live.it>
2025-08-11 13:36:58 +0200

    Create the FeedItem module.

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Vitandrea Sorino on 11/08/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL 
+}
