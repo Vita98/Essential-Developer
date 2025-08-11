ab13d1ce6b28660478dfe669c5efef41fb414ac9
Vitandrea Sorino <vitandrea98@live.it>
2025-08-11 18:25:19 +0200

    First two tests with singleton

---
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,42 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Vitandrea Sorino on 11/08/25.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "http://www.google.it")
+    }
+}
+
+class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
+    var requestedURL: URL?
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequetsDataFromURL() {
+        let client = HTTPClient.shared
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDayaFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
+
+}
